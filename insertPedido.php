00138786ef1fdcfc76c09dd745a7a358d70a2b9c
FSOFTSistemas <fsoftsistemas@gmail.com>
2022-12-19 09:18:14 -0300

    testando deploy hostinger

--- insertPedido.php
+++ insertPedido.php
@@ -1,39 +1,40 @@
     <?php
-        
 
-        class InserirPedido{
 
-            public function inserir($descricao){
-                include "bd.php";
-                $status = "Pendente";
-                $entregador = " ";
-                $timezone = new DateTimeZone('America/Sao_Paulo');
-                $data_atual = new DateTime('now', $timezone);
-                $data_atual = $data_atual->format('Y-m-d H:i:s');
+    class InserirPedido
+    {
 
-                $sql_insert = "INSERT INTO Entrega(descricao, status, entregador, data) VALUES (:DESCRICAO, :STATUSS, :ENTREGADOR, :DATA)";
+        public function inserir($descricao)
+        {
+            include "bd.php";
+            $status = "Pendente";
+            $entregador = " ";
+            $timezone = new DateTimeZone('America/Sao_Paulo');
+            $data_atual = new DateTime('now', $timezone);
+            $data_atual = $data_atual->format('Y-m-d H:i:s');
 
-                $stmt = $PDO->prepare($sql_insert);
-                
-                $stmt->bindParam(':DESCRICAO', $descricao);
-                $stmt->bindParam(':STATUSS', $status);
-                $stmt->bindParam(':ENTREGADOR', $entregador);
-                $stmt->bindParam(':DATA', $data_atual);
+            $sql_insert = "INSERT INTO Entrega(descricao, status, entregador, data) VALUES (:DESCRICAO, :STATUSS, :ENTREGADOR, :DATA)";
 
-                if($stmt->execute()){
+            $stmt = $PDO->prepare($sql_insert);
 
-                    $dados = array("CREATE"=> "OK");
-                    return true;//Cadastrado com sucesso!
+            $stmt->bindParam(':DESCRICAO', $descricao);
+            $stmt->bindParam(':STATUSS', $status);
+            $stmt->bindParam(':ENTREGADOR', $entregador);
+            $stmt->bindParam(':DATA', $data_atual);
 
-                }else{
+            if ($stmt->execute()) {
+                $dados = array("CREATE" => "OK");
+                return true; //Cadastrado com sucesso!
 
-                    $dados = array("CREATE"=> "ERRO");
-                    return false;//Erro ao cadastrar
-                }
+            } else {
+
+                $dados = array("CREATE" => "ERRO");
+                return false; //Erro ao cadastrar
             }
+        }
         //echo json_encode($dados);
 
-        
+
     }
     ?>
 
